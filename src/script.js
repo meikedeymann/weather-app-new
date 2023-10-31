a33d9fba4b641dc59e280bfa912e8b9a23a1b188
meikedeymann <144018923+meikedeymann@users.noreply.github.com>
2023-10-31 12:22:47 +0100

    setup api connection for temperature, city, humidity and wind

--- src/script.js
+++ src/script.js
@@ -1,12 +1,20 @@
 // Functions
 function changeWeather (response) {
-    console.log (response.data.city);
+    curentCity.innerHTML = `${city}`;
+    currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+    currentHumidity.innerHTML = Math.round(response.data.temperature.humidity);
+    currentWind.innerHTML =Math.round(response.data.wind.speed);
 }
 
 // Variables
 let apiKey = "0e0ff2d19bf5c014ad22b48bt938314o";
 let units = "metric";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=${units}`;
+let city = "Hamburg";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+let currentTemperature = document.querySelector(".current-temperature");
+let currentHumidity = document.querySelector(".current-humidity");
+let currentWind = document.querySelector (".current-wind");
+let curentCity = document.querySelector(".current-city");
 
 
 axios.get(`${apiUrl}`).then(changeWeather);
