5061cf5405f7b18c98da6a80f0ed3083a4bcafe3
meikedeymann <144018923+meikedeymann@users.noreply.github.com>
2023-10-31 13:35:13 +0100

    added automatic weather update based on inputs + correction of time update function

--- src/script.js
+++ src/script.js
@@ -1,31 +1,37 @@
 // Functions
+function changeCity (submit){
+    submit.preventDefault();
+    currentCity.innerHTML = `${currentCityInput.value}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${currentCityInput.value}&key=${apiKey}&units=${units}`;
+    axios.get(`${apiUrl}`).then(changeWeather);
+}
+
 function updateTime (){
     if (hour < 10) {
         hour = `0${hours}`;
     }
     if (minutes < 10){
         minutes =`0${minutes}`;
-        dateElement.innerHTML =`${dow} ${hour}:${minutes}`; 
     }
+    dateElement.innerHTML =`${dow} ${hour}:${minutes}`;
 }
 
 function changeWeather (response) {
-    currentCity.innerHTML = `${city}`;
+    currentCity.innerHTML = `${currentCityInput.value}`;
     currentWeatherNarrative.innerHTML = (response.data.condition.description);
     currentTemperature.innerHTML = Math.round(response.data.temperature.current);
     currentHumidity.innerHTML = Math.round(response.data.temperature.humidity);
-    currentWind.innerHTML =Math.round(response.data.wind.speed);
+    currentWind.innerHTML = Math.round(response.data.wind.speed);
 }
 
 // Variables
 let apiKey = "0e0ff2d19bf5c014ad22b48bt938314o";
 let units = "metric";
-let city = "Lisbon";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+let currentCityInput = document.querySelector("#cityInput");
+let currentCity = document.querySelector(".current-city");
 let currentTemperature = document.querySelector(".current-temperature");
 let currentHumidity = document.querySelector(".current-humidity");
 let currentWind = document.querySelector (".current-wind");
-let currentCity = document.querySelector(".current-city");
 let currentWeatherNarrative = document.querySelector(".temperature-narrative");
 let dateElement = document.querySelector (".date-element");
 let now = new Date ();
@@ -33,6 +39,7 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 let dow = days[now.getDay()];
 let hour = now.getHours();
 let minutes = now.getMinutes();
+let searchForm = document.querySelector(".search-form");
 
 updateTime();
-axios.get(`${apiUrl}`).then(changeWeather);
+searchForm.addEventListener("submit", changeCity);
