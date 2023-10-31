8d9173d78466b41d1926cf8cc875cc8c573bb2af
meikedeymann <144018923+meikedeymann@users.noreply.github.com>
2023-10-31 12:46:28 +0100

    added the automatic adjustment of time

--- src/script.js
+++ src/script.js
@@ -1,4 +1,14 @@
 // Functions
+function updateTime (){
+    dateElement.innerHTML =`${dow} ${hour}:${minutes}`; 
+    if (hour<10) {
+        hour = `0${hours}`;
+    }
+    if (minutes<10){
+        minutes =`0${minutes}`;
+    }
+}
+
 function changeWeather (response) {
     currentCity.innerHTML = `${city}`;
     currentWeatherNarrative.innerHTML = (response.data.condition.description);
@@ -17,6 +27,12 @@ let currentHumidity = document.querySelector(".current-humidity");
 let currentWind = document.querySelector (".current-wind");
 let currentCity = document.querySelector(".current-city");
 let currentWeatherNarrative = document.querySelector(".temperature-narrative");
+let dateElement = document.querySelector (".date-element");
+let now = new Date ();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let dow = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
 
-
+updateTime();
 axios.get(`${apiUrl}`).then(changeWeather);
