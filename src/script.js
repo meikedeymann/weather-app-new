2136d7bfe36e4adf98d05545d0779092d7c66b98
meikedeymann <144018923+meikedeymann@users.noreply.github.com>
2023-11-14 12:19:40 +0100

    Final sytling in CSS

--- src/script.js
+++ src/script.js
@@ -66,7 +66,7 @@ function displayForecast (response){
         `
         <div class="weather-forecast-day">
           <div class="weather-forecast-date">${formatDay(day.time)}</div>
-          <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" width="90%"/>
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
               <strong> ${Math.round(day.temperature.maximum)}º </strong> I
